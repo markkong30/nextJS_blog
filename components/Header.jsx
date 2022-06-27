3b5a10e7f25b8d082d500e7567b031f1cea6b32e
markkong30 <markkong30@gmail.com>
2022-06-27 19:46:09 +0100

    fetchPosts

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,30 @@
 import React, { useContext } from 'react';
 import Link from 'next/link';
 
-const Header = () => {
-  return (
-    <div>
+const categories = [{ name: 'react', slug: 'react' }, { name: 'web dev', slug: 'web-dev' }]
 
+
+const Header = () => {
+
+  return (
+    <div className='container flex justify-between items-center mx-auto  mb-8 w-full border-b border-blue-400 py-8'>
+      <div className="">
+        <Link href='/'>
+          <span className="cursor-pointer font-bold text-4xl text-white">
+            Everyday BLOG
+          </span>
+        </Link>
+      </div>
+
+      <div className="hidden md:block">
+        {categories.map(category => (
+          <Link key={category.slug} href={`/category/${category.slug}`}>
+            <span className="mt-2 align-middle text-white ml-4 font-semibolld cursor-pointer">
+              {category.name}
+            </span>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -2,31 +2,35 @@ import Head from "next/head";
 import PostCard from "../components/PostCard";
 import Categories from "../components/Categories";
 import PostWidget from "../components/PostWidget";
+import { fetchPosts } from "../graphQL";
 
-export default function Home() {
-  const posts = [
-    { title: 'React Test', excerpt: 'Learn JEST' },
-    { title: 'React with NextJS', excerpt: 'Learn NextJS' }
-  ]
-
-  return (
-    <div className="container mx-auto px-10 mb-8">
-      <Head>
-        <title>CMS Blog</title>
-      </Head>
-      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-        <div className="lg:col-span-8 col-span-1">
-          {posts.map((post, i) => (
-            <PostCard post={post} key={i} />
-          ))}
-        </div>
-        <div className="lg:col-span-4 col-span-1">
-          <div className="lg:sticky relative top-8">
-            <PostWidget />
-            <Categories />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+export default function Home({ posts }) {
+	return (
+		<div className="container mx-auto px-10 mb-8">
+			<Head>
+				<title>CMS Blog</title>
+			</Head>
+			<div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+				<div className="lg:col-span-8 col-span-1">
+					{posts.map((post, i) => (
+						<PostCard post={post.node} key={i} />
+					))}
+				</div>
+				<div className="lg:col-span-4 col-span-1">
+					<div className="lg:sticky relative top-8">
+						<PostWidget />
+						<Categories />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
+
+export const getStaticProps = async () => {
+	const posts = (await fetchPosts()) || [];
+
+	return {
+		props: { posts },
+	};
+};
